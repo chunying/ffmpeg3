5fdb991b242bd207853107988057432c8861802c
Chun-Ying Huang <huangant@gmail.com>
2017-01-14 18:43:03 +0800

    add openssl module

---
+++ module/openssl.py
@@ -0,0 +1,24 @@
+
+class openssl:
+    name = "openssl 1.1.0c"
+    url = "https://www.openssl.org/source/openssl-1.1.0c.tar.gz"
+    dirname = "" # leave empty to auto guess
+    ffmpeg_opts = [ "--enable-openssl" ];
+
+    def skip(self, prefix, force):
+        if force: return False;
+        if file_exist(prefix + "/bin/openssl"): return True;
+        return False;
+
+    def configure(self, prefix):
+        runcmd("./config --prefix={} --openssldir={}/ssl".format(prefix, prefix));
+
+    def make(self, prefix, opts):
+        runcmd("make {}".format(opts));
+
+    def install(self, prefix):
+        runcmd("make install");
+
+deps.append(openssl());
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
