dc45bebfd8e2f89afdbbaef1a5d9f22a9c6b17ca
Chun-Ying Huang <huangant@gmail.com>
2017-03-03 14:27:33 +0800

    check built-in gcrypt and gpg-error

--- module/gcrypt.py
+++ module/gcrypt.py
@@ -4,6 +4,9 @@ class gcrypt:
     url = "https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.5.tar.bz2"
     dirname = "" # leave empty to auto guess
 
+    def has_builtin(self):
+        return which("libgcrypt-config");
+
     def skip(self, prefix, force):
         if force: return False;
         if file_exist(prefix + "/include/gcrypt.h"): return True;
