a2e8b5a578420e3cb859f04c6c8c685cc6eaeaa6
Chun-Ying Huang <huangant@gmail.com>
2017-03-03 15:51:50 +0800

    add msys2 build support for frei0r

--- module/frei0r.py
+++ module/frei0r.py
@@ -14,13 +14,23 @@ class frei0r:
         return pkg_config_exists("frei0r");
 
     def configure(self, prefix):
+        if msys_sysname() != None:
+            runcmd("mkdir -p build");
+            runcmd("cd build; cmake -G \"MSYS Makefiles\" -DCMAKE_INSTALL_PREFIX={} ..".format(prefix));
+            return;
         runcmd("./autogen.sh");
         runcmd("./configure --prefix={}".format(prefix));
 
     def make(self, prefix, opts):
+        if msys_sysname() != None:
+            runcmd("cd build; make {}".format(opts));
+            return;
         runcmd("make {}".format(opts));
 
     def install(self, prefix):
+        if msys_sysname() != None:
+            runcmd("cd build; make install");
+            return;
         runcmd("make install");
 
 deps.append(frei0r());
